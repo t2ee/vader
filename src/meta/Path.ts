c202124efe8217353309ce38c34e377dd33a7561
Qiaosen Huang <joesonw@gmail.com>
2018-01-25 16:40:43 +0800

    fix @Path

--- src/meta/Path.ts
+++ src/meta/Path.ts
@@ -1,13 +1,8 @@
 import MetadataDecorator from './MetadataDecorator';
 
-function PathDecorator(path: string): (target: any) => any;
-function PathDecorator(path: string): (target: any, key: string, descriptor: TypedPropertyDescriptor<any>) => any;
-function PathDecorator(path: string) {
+export default function Path(path: string) {
     return (target: any, key?: string, descriptor?: TypedPropertyDescriptor<any>) => {
         MetadataDecorator({ path })(target, key);
     };
 }
 
-export default function Path(path: string) {
-    return PathDecorator(path);
-}
